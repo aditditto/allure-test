bf9efc08fcd88592b899de4de6508b712323e79c
Aditya Bimawan <aditditto@gmail.com>
2021-08-22 01:36:14 +0700

    initial commit

---
+++ allure_test/models.py
@@ -0,0 +1,13 @@
+import uuid
+from django.db import models
+
+
+class Product(models.Model):
+    UUID = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False)
+    product_name = models.CharField(max_length=200)
+    product_description = models.TextField()
+    price = models.IntegerField()
+    is_published = models.BooleanField()
+    last_created = models.DateTimeField(editable=False)
+    last_updated = models.DateTimeField(editable=False)
