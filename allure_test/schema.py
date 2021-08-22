d71c6fb382c1cecae03ab1696addf204896907e6
Aditya Bimawan <aditditto@gmail.com>
2021-08-22 23:04:29 +0700

    add support for required graphql queries as in spec

--- allure_test/schema.py
+++ allure_test/schema.py
@@ -17,17 +17,40 @@ class ReviewType(DjangoObjectType):
 
 
 class Query(graphene.ObjectType):
-    products = graphene.List(ProductType)
-    reviews = graphene.List(ReviewType)
-    # product_detail = graphene.Field(
-    #     ProductType, uuid=graphene.UUID(required=True))
-    # product_reviews = graphene.List(ReviewType)
+    products = graphene.List(
+        ProductType, published_only=graphene.Boolean(
+            required=False, default_value=False)
+    )
+    product = graphene.Field(
+        ProductType, uuid=graphene.UUID(required=True),
+        published_only=graphene.Boolean(required=False, default_value=False)
+    )
+    reviews = graphene.List(
+        ReviewType, productUUID=graphene.UUID(required=False),
+        published_only=graphene.Boolean(required=False, default_value=False)
+    )
 
-    def resolve_products(root, info):
-        return Product.objects.all()
+    def resolve_products(root, info, published_only):
+        products = Product.objects.all()
+        if published_only:
+            return products.filter(is_published=True)
+        else:
+            return products
 
-    def resolve_reviews(root, info):
-        return Review.objects.all()
+    def resolve_product(root, info, uuid, published_only):
+        product = Product.objects.get(UUID=uuid)
+        if published_only and not product.is_published:
+            return None
+        else:
+            return product
+
+    def resolve_reviews(root, info, published_only, productUUID=None):
+        reviews = Review.objects.all()
+        if productUUID:
+            reviews = reviews.filter(product=productUUID)
+        if published_only:
+            reviews = reviews.filter(is_published=True)
+        return reviews
 
 
 schema = graphene.Schema(query=Query)
