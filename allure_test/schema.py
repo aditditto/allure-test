8446f8a34e55fd6455179c14b0c8985b700437f6
Aditya Bimawan <aditditto@gmail.com>
2021-08-23 22:22:31 +0700

    add createReview mutation

--- allure_test/schema.py
+++ allure_test/schema.py
@@ -16,6 +16,26 @@ class ReviewType(DjangoObjectType):
         fields = '__all__'
 
 
+class CreateReviewMutation(graphene.Mutation):
+    class Arguments:
+        product_UUID = graphene.UUID(required=True)
+        review_title = graphene.String(required=True)
+        review_text = graphene.String(required=True)
+
+    review = graphene.Field(ReviewType)
+
+    def mutate(root, info, product_UUID, review_title, review_text):
+        try:
+            product = Product.objects.get(UUID=product_UUID)
+        except Product.DoesNotExist:
+            raise Exception("Product with given UUID not found.")
+
+        created = product.review_set.create(
+            review_title=review_title, review_text=review_text, is_published=True)
+
+        return CreateReviewMutation(review=created)
+
+
 class Query(graphene.ObjectType):
     products = graphene.List(
         ProductType, published_only=graphene.Boolean(
@@ -26,7 +46,7 @@ class Query(graphene.ObjectType):
         published_only=graphene.Boolean(required=False, default_value=False)
     )
     reviews = graphene.List(
-        ReviewType, productUUID=graphene.UUID(required=False),
+        ReviewType, product_uuid=graphene.UUID(required=False),
         published_only=graphene.Boolean(required=False, default_value=False)
     )
 
@@ -44,13 +64,17 @@ class Query(graphene.ObjectType):
         else:
             return product
 
-    def resolve_reviews(root, info, published_only, productUUID=None):
+    def resolve_reviews(root, info, published_only, product_uuid=None):
         reviews = Review.objects.all()
-        if productUUID:
-            reviews = reviews.filter(product=productUUID)
+        if product_uuid:
+            reviews = reviews.filter(product=product_uuid)
         if published_only:
             reviews = reviews.filter(is_published=True)
         return reviews
 
 
-schema = graphene.Schema(query=Query)
+class Mutation(graphene.ObjectType):
+    create_review = CreateReviewMutation.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
